c5a10bf4c0ef68fe44cd79aaad287c0a2213e9c4
WeiPeng Yu <yicaifeitian@163.com>
2016-04-15 00:59:05 +0800

    tmx

--- JenkinsTestProject/Form1.Designer.cs
+++ JenkinsTestProject/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.男 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // 男
@@ -42,11 +43,21 @@
             this.男.UseVisualStyleBackColor = true;
             this.男.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.男);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.CheckBox 男;
+        private System.Windows.Forms.Button button1;
     }
 }
 
