44a317af40f7ef03bb8b1efdbb3fe6823c72921b
WeiPeng Yu <yicaifeitian@163.com>
2016-04-15 00:53:31 +0800

    都提交

---
+++ JenkinsTestProject/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace JenkinsTestProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.男 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // 男
+            // 
+            this.男.AutoSize = true;
+            this.男.Location = new System.Drawing.Point(100, 67);
+            this.男.Name = "男";
+            this.男.Size = new System.Drawing.Size(36, 16);
+            this.男.TabIndex = 0;
+            this.男.Text = "男";
+            this.男.UseVisualStyleBackColor = true;
+            this.男.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.男);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox 男;
+    }
+}
+
