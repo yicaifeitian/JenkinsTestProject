e241b594511ba203ce6fe1a5999354040ac07d8c
WeiPeng Yu <yicaifeitian@163.com>
2016-04-17 23:16:51 +0800

    又提交一次。

--- JenkinsTestProject/Form1.Designer.cs
+++ JenkinsTestProject/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.男 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // 男
@@ -52,11 +53,22 @@
             this.button1.Text = "提交";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(142, 67);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 16);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.男);
             this.Name = "Form1";
@@ -71,6 +83,7 @@
 
         private System.Windows.Forms.CheckBox 男;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
